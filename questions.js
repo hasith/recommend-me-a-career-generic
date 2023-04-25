1ce79b29af2cdeca947c81b352260be74520f43e
Hasith Yaggahavita <hasith@gmail.com>
2023-04-25 09:13:27 +0530

    bug fixed and added more careers

---
+++ questions.js
@@ -0,0 +1,369 @@
+const quizData = {
+    "Questions": [
+        {
+            "Caption": "How good are you with numbers?",
+            "Answers": [
+                {
+                    "Caption": "I love numbers and complex math",
+                    "Careers": [
+                        "Data Scientist",
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Script Programmer"
+                    ]
+                },
+                {
+                    "Caption": "I can manage basic math and simple formulas",
+                    "Careers": [
+                        "Automation Engineer",
+                        "DevOps Professional",
+                        "Mobile Developer",
+                        "Security Professional",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I struggle with maths and complex data",
+                    "Careers": [
+                        "UX Professional",
+                        "Software Tester",
+                        "Digital Marketing Professional",
+                        "Web Developer",
+                        "Graphic Designer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you handle stress?",
+            "Answers": [
+                {
+                    "Caption": "I can easily stay productive under pressure",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "DevOps Professional",
+                        "Security Professional",
+                        "Cloud Developer",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I struggle under pressure and need regular breaks",
+                    "Careers": [
+                        "Data Scientist",
+                        "Software Tester",
+                        "Automation Engineer",
+                        "Script Programmer",
+                        "Graphic Designer"
+                    ]
+                },
+                {
+                    "Caption": "I ask for help from others when under pressure",
+                    "Careers": [
+                        "Mobile Developer",
+                        "UX Professional",
+                        "Digital Marketing Professional",
+                        "Web Developer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you solve problems?",
+            "Answers": [
+                {
+                    "Caption": "I break them into smaller parts and solve",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Data Scientist",
+                        "Cloud Developer",
+                        "Script Programmer"
+                    ]
+                },
+                {
+                    "Caption": "I follow known procedures to solve a problem",
+                    "Careers": [
+                        "DevOps Professional",
+                        "Automation Engineer",
+                        "Security Professional",
+                        "Software Tester",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I try different ways and get opinion from others",
+                    "Careers": [
+                        "UX Professional",
+                        "Web Developer",
+                        "Mobile Developer",
+                        "Digital Marketing Professional",
+                        "Graphic Designer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you learn?",
+            "Answers": [
+                {
+                    "Caption": "I learn by doing things hands-on by myself",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Script Programmer",
+                        "Cloud Developer",
+                        "Data Scientist",
+                        "Graphic Designer"
+                    ]
+                },
+                {
+                    "Caption": "I learn by reading and studying available content",
+                    "Careers": [
+                        "Automation Engineer",
+                        "DevOps Professional",
+                        "Security Professional",
+                        "Software Tester",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I learn from others by working together",
+                    "Careers": [
+                        "Mobile Developer",
+                        "Web Developer",
+                        "UX Professional",
+                        "Digital Marketing Professional"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you make decisions?",
+            "Answers": [
+                {
+                    "Caption": "I make decisions quickly and independently",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Data Scientist",
+                        "Security Professional",
+                        "Cloud Developer"
+                    ]
+                },
+                {
+                    "Caption": "I want more infomation to consider all options",
+                    "Careers": [
+                        "DevOps Professional",
+                        "Software Tester",
+                        "Automation Engineer",
+                        "Script Programmer",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I ask others and collaborate on decision making",
+                    "Careers": [
+                        "Mobile Developer",
+                        "UX Professional",
+                        "Digital Marketing Professional",
+                        "Web Developer",
+                        "Graphic Designer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you multitask?",
+            "Answers": [
+                {
+                    "Caption": "I can handle multiple tasks and switch between them easily",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "DevOps Professional",
+                        "Digital Marketing Professional",
+                        "Cloud Developer",
+                        "Mobile Developer",
+                        "Web Developer",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I like to prioritize and focus on one task at a time",
+                    "Careers": [
+                        "Script Programmer",
+                        "Security Professional",
+                        "Data Scientist",
+                        "Automation Engineer",
+                        "UX Professional",
+                        "Software Tester",
+                        "Graphic Designer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you work in teams?",
+            "Answers": [
+                {
+                    "Caption": "I like to privide leadership the team when possible",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Digital Marketing Professional",
+                        "Cloud Developer",
+                        "Data Scientist",
+                    ]
+                },
+                {
+                    "Caption": "I like to play my part in the team and contribute",
+                    "Careers": [
+                        "Web Developer",
+                        "DevOps Professional",
+                        "UX Professional",
+                        "Mobile Developer",
+                        "Software Tester",
+                        "Graphic Designer"
+                    ]
+                },
+                {
+                    "Caption": "I like to work independently to avoid distractions",
+                    "Careers": [
+                        "Script Programmer",
+                        "Automation Engineer",
+                        "Security Professional",
+                        "Systems Administrator"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "What is is your risk taking appetite?",
+            "Answers": [
+                {
+                    "Caption": "I love taking risks and try new solutions",
+                    "Careers": [
+                        "Mobile Developer",
+                        "Digital Marketing Professional",
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "UX Professional"
+                    ]
+                },
+                {
+                    "Caption": "I carefully take calculated risks but always look at practicality",
+                    "Careers": [
+                        "DevOps Professional",
+                        "Script Programmer",
+                        "Web Developer",
+                        "Data Scientist",
+                        "Graphic Designer"
+                    ]
+                },
+                {
+                    "Caption": "I stick to proven approaches and do not like to take risks",
+                    "Careers": [
+                        "Software Tester",
+                        "Automation Engineer",
+                        "Security Professional",
+                        "Systems Administrator"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "What type of work you enjoy most?",
+            "Answers": [
+                {
+                    "Caption": "I'm a perfectionist and like to correct even minor details",
+                    "Careers": [
+                        "Script Programmer",
+                        "Software Tester",
+                        "Security Professional",
+                        "DevOps Professional",
+                        "Automation Engineer",
+                        "Systems Administrator"
+                    ]
+                },
+                {
+                    "Caption": "I like challenging problems and do not enjoy working on repetitive tasks",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Mobile Developer",
+                        "Data Scientist"
+                    ]
+                },
+                {
+                    "Caption": "I like to work on tasks where I can colaborate and understand others",
+                    "Careers": [
+                        "Web Developer",
+                        "Digital Marketing Professional",
+                        "UX Professional",
+                        "Graphic Designer"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How do you look at mistakes and errors?",
+            "Answers": [
+                {
+                    "Caption": "I like to look at them as learning opportunities to improve",
+                    "Careers": [
+                        "Mobile Developer",
+                        "UX Professional",
+                        "Full Stack Developer",
+                        "Cloud Developer",
+                        "Web Developer",
+                        "Digital Marketing Professional"
+                    ]
+                },
+                {
+                    "Caption": "I like to avoid them at all costs and correct immediately if occured",
+                    "Careers": [
+                        "Security Professional",
+                        "Automation Engineer",
+                        "Software Tester",
+                        "DevOps Professional",
+                        "Data Scientist",
+                        "Script Programmer",
+                        "Graphic Designer",
+                        "Systems Administrator"
+                    ]
+                }
+            ]
+        },
+        {
+            "Caption": "How familiar are you with business concepts and enterprise operations?",
+            "Answers": [
+                {
+                    "Caption": "I love learning about different businesses and industries and know a lot about them",
+                    "Careers": [
+                        "Data Scientist",
+                        "UX Professional",
+                        "Digital Marketing Professional",
+                        "Business Analyst"
+                    ]
+                },
+                {
+                    "Caption": "I have some knowledge about business concepts and can understand them",
+                    "Careers": [
+                        "Full Stack Developer",
+                        "Web Developer",
+                        "Graphic Designer",
+                        "Mobile Developer",
+                        "Software Tester",
+                        "Cloud Developer"
+                    ]
+                },
+                {
+                    "Caption": "I have limited knowledge about businesses and not much of an interest",
+                    "Careers": [
+                        "Security Professional",
+                        "Systems Administrator",
+                        "Automation Engineer",
+                        "DevOps Professional",
+                        "Script Programmer",
+                    ]
+                }
+            ]
+        }
+    ]
+}
---
+++ script.js
@@ -0,0 +1,118 @@
+// No changes to the quizData variable in questions.js
+
+const quizForm = document.getElementById("quiz-form");
+const nextBtn = document.getElementById("next");
+const backBtn = document.getElementById("back");
+const tryAgainBtn = document.getElementById("tryAgain");
+const resultEl = document.getElementById("result");
+const careerListEl = document.getElementById("careerList");
+
+let currentQuestion = 0;
+const careerLists = [];
+
+function displayQuestion() {
+    quizForm.innerHTML = ""; // Clear the previous question
+
+    const questionData = quizData.Questions[currentQuestion];
+    const questionContainer = document.createElement("div");
+    const questionCaption = document.createElement("h2");
+
+    questionCaption.textContent = questionData.Caption;
+    questionContainer.appendChild(questionCaption);
+
+    questionData.Answers.forEach((answer, index) => {
+        const answerContainer = document.createElement("div");
+        const input = document.createElement("input");
+        const label = document.createElement("label");
+
+        input.type = "radio";
+        input.name = `question${currentQuestion}`;
+        input.id = `question${currentQuestion}answer${index}`;
+        input.value = index;
+
+        label.htmlFor = `question${currentQuestion}answer${index}`;
+        label.textContent = answer.Caption;
+
+        input.addEventListener("change", () => {
+            nextBtn.disabled = false;
+        });
+
+        answerContainer.appendChild(input);
+        answerContainer.appendChild(label);
+        questionContainer.appendChild(answerContainer);
+    });
+
+    quizForm.appendChild(questionContainer);
+    nextBtn.disabled = true;
+    backBtn.disabled = currentQuestion === 0;
+}
+
+function tryAgain() {
+    currentQuestion = 0;
+    careerLists.length = 0;
+    displayQuestion();
+    resultEl.classList.add("hidden");
+    quizForm.classList.remove("hidden");
+    nextBtn.classList.remove("hidden");
+    backBtn.classList.remove("hidden");
+}
+
+function calculateResults() {
+    const formData = new FormData(quizForm);
+    const userChoices = Array.from(formData.values());
+
+    userChoices.forEach((choice) => {
+        const careers = quizData.Questions[currentQuestion-1].Answers[choice].Careers;
+        careerLists[currentQuestion] = careers;
+    });
+}
+
+function displayResult() {
+    quizForm.classList.add("hidden"); // Hide the questions
+    nextBtn.classList.add("hidden"); // Hide the next button
+    backBtn.classList.add("hidden"); // Hide the back button
+
+    const careerCount = {};
+
+    careerLists.forEach((list) => {
+        list.forEach(career => {
+            careerCount[career] = (careerCount[career] || 0) + 1;
+        });
+    });
+
+    const recommendedCareers = Object.entries(careerCount).sort((a, b) => b[1] - a[1]).slice(0, 5);
+
+    careerListEl.innerHTML = ""; //clear the previous list
+
+    recommendedCareers.forEach(([career]) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = career;
+        careerListEl.appendChild(listItem);
+    });
+
+    resultEl.classList.remove("hidden");
+}
+
+nextBtn.addEventListener("click", () => {
+    if (quizForm.reportValidity()) {
+        currentQuestion++;
+
+        if (currentQuestion < quizData.Questions.length) {
+            calculateResults()
+            displayQuestion();
+        } else {
+            displayResult();
+        }
+    }
+});
+
+backBtn.addEventListener("click", () => {
+    if (currentQuestion > 0) {
+        currentQuestion--;
+        displayQuestion();
+    }
+});
+
+tryAgainBtn.addEventListener("click", tryAgain);
+
+displayQuestion();
