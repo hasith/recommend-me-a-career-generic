45ad1593cea09d1652b65a389e62737e4c0b63bc
Hasith Yaggahavita <hasith@gmail.com>
2023-09-01 11:27:26 +0530

    adding question number

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ function displayQuestion() {
 
     const questionData = quizData.Questions[currentQuestion];
     const questionContainer = document.createElement("div");
+    const questionNumber = document.createElement("h3");
     const questionCaption = document.createElement("h2");
 
+    const index = currentQuestion + 1;
+    questionNumber.textContent = "Question " + index + "/" + quizData.Questions.length;
     questionCaption.textContent = questionData.Caption;
+    questionContainer.appendChild(questionNumber);
     questionContainer.appendChild(questionCaption);
 
     questionData.Answers.forEach((answer, index) => {
